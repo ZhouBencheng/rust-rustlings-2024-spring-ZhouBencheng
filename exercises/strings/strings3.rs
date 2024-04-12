4bd6ce9cd641e443afb5248d39671c2c535b402d
Ben <zhoubencheng2022@163.com>
2024-04-12 10:42:25 +0800

    update

--- exercises/strings/strings3.rs
+++ exercises/strings/strings3.rs
@@ -12,7 +12,7 @@ fn trim_me(input: &str) -> String {
 
 fn compose_me(input: &str) -> String {
     // TODO: Add " world!" to the string! There's multiple ways to do this!
-    ???
+    String::from(input).push_str(" world!")
 }
 
 fn replace_me(input: &str) -> String {
